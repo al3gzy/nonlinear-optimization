6c20a7e5d8e37bed402e40cee6b0eab226d2d039
Aleksa Milovanović <166875581+al3gzy@users.noreply.github.com>
2025-08-02 13:29:23 +0200

    Update njutn_fiksan.m

--- 1/njutn_fiksan.m
+++ 1/njutn_fiksan.m
@@ -3,7 +3,7 @@ function [nizx, nizf] = njutn_fiksan(x0, eps, f, df, ddf)
     nizx = [x0];
     nizf = [f(x0)];
     xk = x0;
-    alfa = 0.2;
+    alfa = 0.05;
 
     while norm(df(xk)) > eps
         dk = -ddf(xk) \ df(xk);
